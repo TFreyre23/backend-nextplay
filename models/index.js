039dd7d35f082befd0fe5ad26448be8f55449c17
Tomás Freyre <freyre@uc.cl>
2024-12-07 19:37:34 -0300

    Agregando cambios en index.js

--- models/index.js
+++ models/index.js
@@ -10,11 +10,29 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+sequelize = new Sequelize({
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  dialectOptions: {
+    ssl: {
+      require: true, // Ensure SSL is required
+      rejectUnauthorized: false // Set to true for stricter security, false if you trust the source and want to bypass certificate verification
+    }
+  }
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 
 fs
   .readdirSync(__dirname)
